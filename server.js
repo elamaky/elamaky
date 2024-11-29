cba6e4fd1d967980723434d53de0863eed8b5aa3
strsljennik <strsljennik@gmail.com>
2024-11-29 13:31:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ io.on('connection', (socket) => {
     console.log(`${nickname} se povezao.`);
 
     // Emitovanje događaja da bi ostali korisnici videli novog gosta
-    socket.broadcast.emit('newGuest', nickname);
     const updatedGuests = ensureRadioGalaksijaAtTop(guests);
+    socket.broadcast.emit('newGuest', nickname);
     io.emit('updateGuestList', Object.values(guests));
 
     // Obrada prijave korisnika
