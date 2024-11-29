75477d84f2132d15f2d236e088ac5b79fd8b742b
strsljennik <strsljennik@gmail.com>
2024-11-29 12:41:44 +0100

    Update sitnice.js

--- sitnice.js
+++ sitnice.js
@@ -1,6 +1,7 @@
 const moment = require('moment-timezone');
 const lastMessages = {}; // Objekat za čuvanje poslednjih poruka
 
+// Funkcija za obezbeđivanje da 'Radio Galaksija' bude na vrhu liste gostiju
 function ensureRadioGalaksijaAtTop(guests) {
     const guestList = Object.values(guests);
     if (guestList.includes('Radio Galaksija')) {
@@ -11,14 +12,15 @@ function ensureRadioGalaksijaAtTop(guests) {
     return guestList;
 }
 
-socket.on('chatMessage', (msgData) => {
+// Funkcija koja se poziva prilikom slanja poruka
+function handleChatMessage(socket, msgData) {
     // Ignoriši ako je poruka identična
-    if (lastMessages[socket.id] === msgData.text) return; 
+    if (lastMessages[socket.id] === msgData.text) return;
     lastMessages[socket.id] = msgData.text; // Sačuvaj novu poruku
 
     // Obrada #n komande
     if (msgData.text.includes('#n')) {
-        const nickname = getNickname(socket.id); // Funkcija koja dobija nadimak korisnika
+        const nickname = getNickname(socket.id); // Pretpostavljamo da funkcija getNickname postoji
         const message = `${nickname} CITA POZ ${nickname}`;
         io.emit('chatMessage', message);
     }
@@ -38,8 +40,14 @@ socket.on('chatMessage', (msgData) => {
         const timeMessage = `Vreme u Berlinu: ${berlinTime}`;
         io.emit('chatMessage', timeMessage); // Emituj vreme u berlinskoj zoni
     }
+}
+
+// Registracija događaja kada korisnik pošalje poruku
+socket.on('chatMessage', (msgData) => {
+    handleChatMessage(socket, msgData);
 });
 
+// Eksport funkcije
 module.exports = {
     ensureRadioGalaksijaAtTop
 };
