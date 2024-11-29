098b180edd4648bf09d19c4e73eea477ef8d7a60
strsljennik <strsljennik@gmail.com>
2024-11-29 09:54:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const socketIo = require('socket.io');
 const { connectDB } = require('./mongo');
 const { register, login } = require('./prijava');
 const { setupSocketEvents } = require('./banmodul'); // Uvoz funkcije iz banmodula
-const uuidRouter = require('./uuidmodul'); // Putanja do modula
-const { saveIpData, getIpData } = require('./ip'); // Uvozimo ip.js
 const pingService = require('./ping');
 require('dotenv').config();
 
@@ -18,8 +16,6 @@ connectDB(); // Povezivanje na bazu podataka
 // Middleware za parsiranje JSON podataka i serviranje statičkih fajlova
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-app.use('/guests', uuidRouter); // Dodavanje ruta u aplikaciju
-app.set('trust proxy', true);
 
 // Rute za registraciju i prijavu
 app.post('/register', (req, res) => register(req, res, io));
@@ -30,13 +26,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// Lista autorizovanih korisnika i banovanih korisnika
+const authorizedUsers = new Set(['Radio Galaksija', 'ZI ZU', '__X__']);
+const bannedUsers = new Set();
+
 // Skladištenje informacija o gostima
 const guests = {};
 const assignedNumbers = new Set(); // Set za generisane brojeve
-const bannedUsers = new Set(); // Set za banovane korisnike
 
 // Dodavanje socket događaja iz banmodula
-setupSocketEvents(io, guests, bannedUsers); // Prosleđujemo bannedUsers
+setupSocketEvents(io, guests, bannedUsers); // Dodavanje guests i bannedUsers u banmodul
 
 // Socket.io događaji
 io.on('connection', (socket) => {
@@ -71,12 +70,8 @@ io.on('connection', (socket) => {
             italic: msgData.italic,
             color: msgData.color,
             nickname: guests[socket.id], // Korišćenje nadimka za slanje poruke
-            time: time
+            time: time,
         };
-
-        // Spremi IP, poruku i nickname u fajl
-        saveIpData(socket.handshake.address, msgData.text, guests[socket.id]);
-        
         io.emit('chatMessage', messageToSend);
     });
 
@@ -87,15 +82,17 @@ io.on('connection', (socket) => {
         io.emit('updateGuestList', Object.values(guests));
     });
 
-    // Mogućnost banovanja korisnika prema socket.id
-    socket.on('banUser', (socketIdToBan) => {
-        if (bannedUsers.has(socketIdToBan)) {
+    // Mogućnost banovanja korisnika prema nickname-u
+    socket.on('banUser', (nicknameToBan) => {
+        const socketIdToBan = Object.keys(guests).find(key => guests[key] === nicknameToBan);
+
+        if (socketIdToBan) {
             io.to(socketIdToBan).emit('banned');
             io.sockets.sockets[socketIdToBan].disconnect();
-            console.log(`Korisnik sa ID ${socketIdToBan} je banovan.`);
+            console.log(`Korisnik ${nicknameToBan} (ID: ${socketIdToBan}) je banovan.`);
         } else {
-            console.log(`Korisnik sa ID ${socketIdToBan} nije pronađen.`);
-            socket.emit('userNotFound', socketIdToBan);
+            console.log(`Korisnik ${nicknameToBan} nije pronađen.`);
+            socket.emit('userNotFound', nicknameToBan);
         }
     });
 
