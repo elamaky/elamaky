e6178d5faf6f947c4dc3e8ccaa1dce4071ac8a35
strsljennik <strsljennik@gmail.com>
2024-11-29 12:53:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { register, login } = require('./prijava');
 const { setupSocketEvents } = require('./banmodul'); // Uvoz funkcije iz banmodula
 const uuidRouter = require('./uuidmodul'); // Putanja do modula
 const { saveIpData, getIpData } = require('./ip'); // Uvozimo ip.js
-const { ensureRadioGalaksijaAtTop } = require('./sitnice'); // Uvoz funkcije iz sitnice.js
+const { ensureRadioGalaksijaAtTop, handleChatMessage } = require('./sitnice'); // Uvoz funkcije iz sitnice.js
 const pingService = require('./ping');
 require('dotenv').config();
 
@@ -44,18 +44,22 @@ setupSocketEvents(io, guests, bannedUsers); // Dodavanje guests i bannedUsers u 
 
 // Socket.io događaji
 io.on('connection', (socket) => {
+    const ipAddress = socket.handshake.address; // Dobijamo IP adresu korisnika
+    saveIpData(socket.id, ipAddress); // Sačuvaj IP adresu u bazu ili neki drugi storage
+
     // Generisanje jedinstvenog broja za gosta
     const uniqueNumber = generateUniqueNumber();
     const nickname = `Gost-${uniqueNumber}`; // Nadimak korisnika
     guests[socket.id] = nickname; // Dodajemo korisnika u guest list
-    console.log(`${nickname} se povezao.`);
+    console.log(`${nickname} se povezao sa IP adresom: ${ipAddress}`);
 
     // Ažuriranje liste gostiju
-    const updatedGuests = ensureRadioGalaksijaAtTop(guests);
+    const updatedGuests = ensureRadioGalaksijaAtTop(guests);  
     io.emit('updateGuestList', updatedGuests);  // Emituj ažuriranu listu
 
     // Emitovanje događaja da bi ostali korisnici videli novog gosta
     socket.broadcast.emit('newGuest', nickname);
+    io.emit('updateGuestList', updatedGuests);  // Emituj ažuriranu listu
 
     // Obrada prijave korisnika
     socket.on('userLoggedIn', async (username) => {
@@ -71,19 +75,7 @@ io.on('connection', (socket) => {
 
     // Obrada slanja poruka u četu
     socket.on('chatMessage', (msgData) => {
-        const time = new Date().toLocaleTimeString();
-        const messageToSend = {
-            text: msgData.text,
-            bold: msgData.bold,
-            italic: msgData.italic,
-            color: msgData.color,
-            nickname: guests[socket.id], // Korišćenje nadimka za slanje poruke
-            time: time,
-        };
-        // Spremi IP, poruku i nickname u fajl
-        saveIpData(socket.handshake.address, msgData.text, guests[socket.id]);
-
-        io.emit('chatMessage', messageToSend);
+        handleChatMessage(io, socket, msgData); // Pozivanje funkcije sa socketom
     });
 
     // Obrada diskonekcije korisnika
