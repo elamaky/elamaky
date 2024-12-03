7984457424bc1fa09ce9107c73100ebf6e826244
strsljennik <strsljennik@gmail.com>
2024-12-03 23:27:15 +0100

    Update slike.js

--- public/slike.js
+++ public/slike.js
@@ -56,33 +56,19 @@ document.getElementById('addImage').addEventListener('click', function() {
             img.style.height = "200px"; 
             img.style.position = "absolute"; 
             img.style.zIndex = "1000";  
+            img.classList.add('draggable', 'resizable');  
             img.style.border = "none"; // Ukloni border po defaultu
             img.style.display = 'block'; // Dodajemo 'block' kako bi slika bila vidljiva
+            img.style.pointerEvents = "none"; // Zaštita od slučajnog klikanja na sliku
             document.body.appendChild(img);
             enableDragAndResize(img);
-            
-            // Emitovanje slike svim korisnicima
-            socket.emit('add-image', imageSource);
+            console.log("Slika je dodata preko URL-a.");
         } else {
             alert("Nepodržan format slike. Podržani formati su: JPG, PNG, GIF.");
         }
     } else {
         alert("Niste uneli URL slike.");
     }
-});
-
-// Kada server emituj sliku, klijent treba da je prikaže
-socket.on('display-image', (imageSource) => {
-    const img = document.createElement('img');
-    img.src = imageSource;  
-    img.style.width = "200px";  
-    img.style.height = "200px"; 
-    img.style.position = "absolute"; 
-    img.style.zIndex = "1000";  
-    img.style.border = "none"; 
-    img.style.display = 'block'; 
-    img.style.pointerEvents = "none"; // Onemogućava interakciju sa slikom za druge korisnike
-    document.body.appendChild(img);
 });
 
 function enableDragAndResize(img) {
@@ -170,3 +156,10 @@ function enableDragAndResize(img) {
         document.onmousemove = null;
     }
 }
+
+// Slušanje na 'chat-cleared' događaj
+socket.on('chat-cleared', function() {
+    console.log('Chat je obrisan sa servera.');
+    const chatWindow = document.getElementById('messageArea');
+    chatWindow.innerHTML = ""; // Briše sve unutar chata
+});
