bfd4152eb80c30c17a7a8c1b451a8c9d1a8c0106
strsljennik <strsljennik@gmail.com>
2024-12-04 18:26:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -85,12 +85,7 @@ socket.on('update-image', (data) => {
     io.emit('sync-image', data);  // Emitovanje promjena svim klijentima
 });
 
-        
-});
-
-
-
-    // Funkcije iz modula poruke.js
+     // Funkcije iz modula poruke.js
     setSocket(socket, io);  // Inicijalizacija socket-a i io objekta
     chatMessage(guests);     // Pokretanje funkcije za slanje poruka
     clearChat();            // Pokretanje funkcije za brisanje chata
