aff83f378cfb72fc6d3bf2e33533e920a15d7840
strsljennik <strsljennik@gmail.com>
2024-12-04 17:13:43 +0100

    Update slike.js

--- public/slike.js
+++ public/slike.js
@@ -64,28 +64,6 @@ document.getElementById('addImage').addEventListener('click', function () {
                 addImageToDOM(imageUrl);  // Prikaz nove slike koju je server poslao
             });
 
-            // Prikaz slike odmah nakon što je dodata sa URL-a
-            const img = document.createElement('img');
-            img.src = imageSource;
-            console.log("Slika URL:", img.src);
-            img.style.width = "200px";
-            img.style.height = "200px";
-            img.style.position = "absolute";
-            img.style.zIndex = "1000";
-            img.classList.add('draggable', 'resizable');
-            img.style.border = "none";
-            img.style.display = 'block';
-            document.body.appendChild(img);
-            enableDragAndResize(img);
-            console.log("Slika je dodata preko URL-a.");
-        } else {
-            alert("Nepodržan format slike. Podržani formati su: JPG, PNG, GIF.");
-        }
-    } else {
-        alert("Niste uneli URL slike.");
-    }
-});
-
 // Prikaz svih prethodnih slika kad se poveže klijent
 socket.on('initial-images', (images) => {
     images.forEach(addImageToDOM);  // Dodaj sve slike koje su već dodate
@@ -97,7 +75,10 @@ function addImageToDOM(imageUrl) {
     img.src = imageUrl;
     img.style.width = "200px";
     img.style.height = "200px";
+    img.style.position = "absolute";
+    img.style.zIndex = "1000";
     img.classList.add('draggable', 'resizable');
+    img.style.border = "none";
     document.body.appendChild(img);
     enableDragAndResize(img);  // Ako postoji funkcija za povlačenje i promenu veličine
 }
