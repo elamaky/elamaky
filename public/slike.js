bbac484dd5346d7af7ca01a7431d02e719951670
strsljennik <strsljennik@gmail.com>
2024-12-05 15:53:14 +0100

    Update slike.js

--- public/slike.js
+++ public/slike.js
@@ -183,6 +183,11 @@ function enableDragAndResize(img) {
         document.onmousemove = null;
     }
 }
+socket.emit('update-image', {
+    imageUrl: img.src,
+    position: { x: img.style.left, y: img.style.top },
+    dimensions: { width: img.style.width, height: img.style.height }
+});
 
 socket.on('sync-image', (data) => {
     const img = document.querySelector(`img[src="${data.imageUrl}"]`);
