d5125c554bcf696f6a3a50ec41d26f55f7cd5185
strsljennik <strsljennik@gmail.com>
2024-12-07 01:27:27 +0100

    Update slike.js

--- public/slike.js
+++ public/slike.js
@@ -18,21 +18,8 @@ document.getElementById('addImage').addEventListener('click', function () {
                 addImageToDOM(imageUrl);  // Prikaz nove slike koju je server poslao
             });
 
-        } else {
-            alert("Nepodržan format slike. Podržani formati su: JPG, PNG, GIF.");
-        }
-    } else {
-        alert("Niste uneli URL slike.");
-    }
-});
-
-// Prikaz svih prethodnih slika kad se poveže klijent
-socket.on('initial-images', (images) => {
-    images.forEach(addImageToDOM);  // Dodaj sve slike koje su već dodate
-});
-
-// Funkcija za dodavanje slike u DOM
-function addImageToDOM(imageUrl) {
+      // Funkcija za dodavanje slike u DOM
+    function addImageToDOM(imageUrl) {
     currentImage = document.createElement('img'); // Zamenjeno img sa currentImage
     currentImage.src = imageUrl;
     currentImage.style.width = "200px";
