c097f5f53270fc8748d6acec744348d64d73488b
strsljennik <strsljennik@gmail.com>
2024-12-08 16:30:25 +0100

    Update poruke.js

--- poruke.js
+++ poruke.js
@@ -4,6 +4,7 @@ const queryString = require('query-string'); // Importovanje query-string
 let socket; // Inicijalizujemo socket
 
 const currentImages = []; // Skladištenje URL-ova slika, pozicija i dimenzije
+const newImage;
 
 // Funkcija za setovanje socket-a i io objekta
 function setSocket(serverSocket, serverIo) {
