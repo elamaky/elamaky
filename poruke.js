e1659aacbb0fbe0ea26da3c300e4c6a94b8bafbf
strsljennik <strsljennik@gmail.com>
2024-12-10 12:28:58 +0100

    Update poruke.js

--- poruke.js
+++ poruke.js
@@ -1,74 +1,68 @@
 let io;
-let socket;
-let newImage = [];  
+let newImage = [];
 
 // Funkcija za setovanje socket-a i io objekta
 function setSocket(serverSocket, serverIo) {
-    socket = serverSocket;
     io = serverIo;
 
-    io.on('connection', (socket) => {
-        socket.emit('initial-images', newImage);
-    });
+    io.on('connection', (clientSocket) => {
+        clientSocket.emit('initial-images', newImage);
 
-    socket.on('add-image', (imageSource, position, dimensions) => {
-        if (!imageSource || !position || !dimensions) return;
+        clientSocket.on('add-image', (imageSource, position, dimensions) => {
+            if (!imageSource || !position || !dimensions) return;
 
-        newImage.push({
-            imageUrl: imageSource,
-            position: position,
-            dimensions: dimensions
+            newImage.push({
+                imageUrl: imageSource,
+                position: position,
+                dimensions: dimensions
+            });
+
+            io.emit('display-image', {
+                imageUrl: imageSource,
+                position: position,
+                dimensions: dimensions
+            });
         });
 
-        io.emit('display-image', {
-            imageUrl: imageSource,
-            position: position,
-            dimensions: dimensions
+        clientSocket.on('update-image', (data) => {
+            const image = newImage.find(img => img.imageUrl === data.imageUrl);
+            if (image) {
+                image.position = data.position;
+                image.dimensions = data.dimensions;
+            }
+            io.emit('sync-image', data);
         });
-    });
 
-    socket.on('update-image', (data) => {
-        const image = newImage.find(img => img.imageUrl === data.imageUrl);
-        if (image) {
-            image.position = data.position;
-            image.dimensions = data.dimensions;
-        }
-        io.emit('sync-image', data);
-    });
+        clientSocket.on('remove-image', (imageUrl) => {
+            const index = newImage.findIndex(img => img.imageUrl === imageUrl);
+            if (index !== -1) {
+                newImage.splice(index, 1);
+            }
+            io.emit('update-images', newImage);
+        });
 
+        // Obrada chat poruka
+        clientSocket.on('chatMessage', (msgData) => {
+            const time = new Date().toLocaleTimeString();
+            const messageToSend = {
+                text: msgData.text,
+                bold: msgData.bold,
+                italic: msgData.italic,
+                color: msgData.color,
+                nickname: msgData.nickname || 'Guest', // Dodajte provere za guests
+                time: time
+            };
+            io.emit('chatMessage', messageToSend);
+        });
 
-    socket.on('remove-image', (imageUrl) => {
-        const index = newImage.findIndex(img => img.imageUrl === imageUrl);
-        if (index !== -1) {
-            newImage.splice(index, 1);
-        }
-        io.emit('update-images', newImage);
-    });
-}
-
-// Funkcija za obradu slanja poruka u četu
-function chatMessage(guests) {
-    socket.on('chatMessage', (msgData) => {
-        const time = new Date().toLocaleTimeString();
-        const messageToSend = {
-            text: msgData.text,
-            bold: msgData.bold,
-            italic: msgData.italic,
-            color: msgData.color,
-            nickname: guests[socket.id], // Dodajte provere za guests
-            time: time
-        };
-        io.emit('chatMessage', messageToSend);
-    });
-}
-
-// Funkcija za brisanje chata
-function clearChat() {
-    socket.on('clear-chat', () => {
-        console.log(`Zahtev za brisanje chata primljen od ${socket.id}`);
-        io.emit('chat-cleared'); // Emituj svim korisnicima da je chat obrisan
+        // Brisanje chata
+        clientSocket.on('clear-chat', () => {
+            console.log(`Zahtev za brisanje chata primljen od ${clientSocket.id}`);
+            io.emit('chat-cleared');
+        });
     });
 }
 
 // Eksportovanje funkcija
-module.exports = { setSocket, chatMessage, clearChat };
+module.exports = { setSocket };
+
