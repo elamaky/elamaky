a4f28beea0bc7fb50788aeaab4bd265f7c4d9325
strsljennik <strsljennik@gmail.com>
2024-12-10 18:33:09 +0100

    Update poruke.js

--- poruke.js
+++ poruke.js
@@ -1,120 +1,86 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const { connectDB } = require('./mongo');
-const { register, login } = require('./prijava');
-const { setupSocketEvents } = require('./banmodul'); // Uvoz funkcije iz banmodula
-const { saveIpData, getIpData } = require('./ip'); // Uvozimo ip.js
-const uuidRouter = require('./uuidmodul'); // Putanja do modula
-const { ensureRadioGalaksijaAtTop } = require('./sitnice');
-const konobaricaModul = require('./konobaricamodul');
-const { setSocket, chatMessage, clearChat } = require('./poruke');
-const pingService = require('./ping');
-require('dotenv').config();
+let io; // Inicijalizujemo io
+let socket; // Inicijalizujemo socket
+const newImage = []; // Skladištenje URL-ova slika, pozicija i dimenzije
 
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : '*',
-        methods: ["GET", "POST"],
-        credentials: true
-    }
-});
+// Funkcija za setovanje socket-a i io objekta
+function setSocket(serverSocket, serverIo) {
+    socket = serverSocket;
+    io = serverIo;
 
-connectDB(); // Povezivanje na bazu podataka
-konobaricaModul(io);
+    io.on('connection', (socket) => {
+        // Emitujemo sve slike kada se novi klijent poveže
+        socket.emit('initial-images', newImage); // Ovdje šaljemo postojeće slike
 
-// Middleware za parsiranje JSON podataka i serviranje statičkih fajlova
-app.use(express.json());
-app.use(express.static(__dirname + '/public'));
-app.use('/guests', uuidRouter); // Dodavanje ruta u aplikaciju
-app.set('trust proxy', true);
+        // Osluskivanje dodatnih događaja
+        chatMessage();
+        clearChat();
+    });
 
-// Rute za registraciju i prijavu
-app.post('/register', (req, res) => register(req, res, io));
-app.post('/login', (req, res) => login(req, res, io));
-
-// Početna ruta
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-// Lista autorizovanih korisnika i banovanih korisnika
-const authorizedUsers = new Set(['Radio Galaksija', 'ZI ZU', '__X__']);
-const bannedUsers = new Set();
-
-// Skladištenje informacija o gostima
-const guests = {};
-const assignedNumbers = new Set(); // Set za generisane brojeve
-
-// Dodavanje socket događaja iz banmodula
-setupSocketEvents(io, guests, bannedUsers); // Dodavanje guests i bannedUsers u banmodul
-
-// Socket.io događaji
-io.on('connection', (socket) => {
-    const uniqueNumber = generateUniqueNumber();
-    const nickname = `Gost-${uniqueNumber}`; // Nadimak korisnika
-    guests[socket.id] = nickname; // Dodajemo korisnika u guest list
-    console.log(`${nickname} se povezao.`);
-
-    // Emitovanje događaja da bi ostali korisnici videli novog gosta
-    const updatedGuests = ensureRadioGalaksijaAtTop(guests);
-    socket.broadcast.emit('newGuest', nickname);
-    io.emit('updateGuestList', Object.values(guests));
-
-    // Obrada prijave korisnika
-    socket.on('userLoggedIn', async (username) => {
-        if (authorizedUsers.has(username)) {
-            guests[socket.id] = username; // Admin
-            console.log(`${username} je autentifikovan kao admin.`);
-        } else {
-            guests[socket.id] = username; // Običan gost
-            console.log(`${username} se prijavio kao gost.`);
+    // Osluškujemo kada klijent doda novu sliku
+    socket.on('add-image', (imageSource, position, dimensions) => {
+        if (!imageSource || !position || !dimensions) {
+            console.error('Greška: Nedostaju podaci slike');
+            return;
         }
-        io.emit('updateGuestList', Object.values(guests));
+
+        console.log('Dodata slika:', imageSource, 'Pozicija:', position, 'Dimenzije:', dimensions);
+
+        // Dodajemo sliku u listu sa pozicijom i dimenzijama
+        newImage.push({
+            imageUrl: imageSource,
+            position: position,
+            dimensions: dimensions
+        });
+
+        // Emitujemo sliku svim klijentima
+        console.log('Emitovanje slike svim klijentima:', newImage);
+        io.emit('display-image', {
+            imageUrl: imageSource,
+            position: position,
+            dimensions: dimensions
+        });
     });
 
-    // Funkcije iz modula poruke.js
-    setSocket(socket, io);  // Inicijalizacija socket-a i io objekta
-    chatMessage(guests);     // Pokretanje funkcije za slanje poruka
-    clearChat();            // Pokretanje funkcije za brisanje chata
+    // Kada server primi 'update-image' događaj od klijenta
+    socket.on('update-image', (data) => {
+        console.log('Primljeni podaci za update slike:', data);
 
-
-  // Obrada diskonekcije korisnika
-    socket.on('disconnect', () => {
-        console.log(`${guests[socket.id]} se odjavio.`);
-        delete guests[socket.id]; // Uklanjanje gosta iz liste
-        io.emit('updateGuestList', Object.values(guests));
-    });
-
-   // Mogućnost banovanja korisnika prema nickname-u
-    socket.on('banUser', (nicknameToBan) => {
-        const socketIdToBan = Object.keys(guests).find(key => guests[key] === nicknameToBan);
-
-        if (socketIdToBan) {
-            io.to(socketIdToBan).emit('banned');
-            io.sockets.sockets[socketIdToBan].disconnect();
-            console.log(`Korisnik ${nicknameToBan} (ID: ${socketIdToBan}) je banovan.`);
-        } else {
-            console.log(`Korisnik ${nicknameToBan} nije pronađen.`);
-            socket.emit('userNotFound', nicknameToBan);
+        // Ažuriramo sliku u `newImage` listi
+        const image = newImage.find(img => img.imageUrl === data.imageUrl);
+        if (image) {
+            image.position = data.position;
+            image.dimensions = data.dimensions;
         }
-    });
-});
 
-// Funkcija za generisanje jedinstvenog broja
-function generateUniqueNumber() {
-    let number;
-    do {
-        number = Math.floor(Math.random() * 8889) + 1111; // Brojevi između 1111 i 9999
-    } while (assignedNumbers.has(number));
-    assignedNumbers.add(number);
-    return number;
+        // Emitujemo promene svim ostalim klijentima
+        io.emit('sync-image', data);
+        console.log('Podaci emitovani drugim klijentima:', data);
+    });
 }
 
-// Pokretanje servera na definisanom portu
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server je pokrenut na portu ${PORT}`);
-});
+// Funkcija za obradu slanja poruka u četu
+function chatMessage() {
+    socket.on('chatMessage', (msgData) => {
+        const time = new Date().toLocaleTimeString();
+        const messageToSend = {
+            text: msgData.text,
+            bold: msgData.bold,
+            italic: msgData.italic,
+            color: msgData.color,
+            nickname: guests[socket.id],
+            time: time
+        };
+        io.emit('chatMessage', messageToSend);
+    });
+}
+
+// Funkcija za brisanje chata
+function clearChat() {
+    socket.on('clear-chat', () => {
+        console.log(`Zahtev za brisanje chata primljen od ${socket.id}`);
+        io.emit('chat-cleared'); // Emituj svim korisnicima da je chat obrisan
+    });
+}
+
+// Eksportovanje funkcija
+module.exports = { setSocket, chatMessage, clearChat };
