e8eeba543a54c32d4c97a18d7fc1dee76d2f00e7
strsljennik <strsljennik@gmail.com>
2024-12-10 12:17:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -75,11 +75,26 @@ io.on('connection', (socket) => {
         io.emit('updateGuestList', Object.values(guests));
     });
 
-    // Funkcije iz modula poruke.js
-    setSocket(socket, io);  // Inicijalizacija socket-a i io objekta
-    chatMessage(guests);     // Pokretanje funkcije za slanje poruka
-    clearChat();            // Pokretanje funkcije za brisanje chata
+ socket.on('chatMessage', (msgData) => {
+        const time = new Date().toLocaleTimeString();
+        const messageToSend = {
+            text: msgData.text,
+            bold: msgData.bold,
+            italic: msgData.italic,
+            color: msgData.color,
+            nickname: guests[socket.id],
+            time: time
+        };
+        io.emit('chatMessage', messageToSend);
+    });
 
+// Funkcija za brisanje chata
+function clearChat() {
+    socket.on('clear-chat', () => {
+        io.emit('chat-cleared');
+    });
+}
+    
 
   // Obrada diskonekcije korisnika
     socket.on('disconnect', () => {
