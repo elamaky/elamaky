cb5b118769ae9424bbd638a6d39c1017b93faba9
strsljennik <strsljennik@gmail.com>
2024-12-10 23:28:32 +0100

    Update slikemodul.js

--- slikemodul.js
+++ slikemodul.js
@@ -1,47 +1,45 @@
 let io;
-let socket;
 let newImage = [];  
 
-// Funkcija za setovanje socket-a i io objekta
-function setSocket(serverSocket, serverIo) {
-    socket = serverSocket;
+// Funkcija za setovanje io objekta
+function setSocket(serverIo) {
     io = serverIo;
 
     io.on('connection', (socket) => {
         socket.emit('initial-images', newImage);
-    });
 
-    socket.on('add-image', (imageSource, position, dimensions) => {
-        if (!imageSource || !position || !dimensions) return;
+        socket.on('add-image', (imageSource, position, dimensions) => {
+            if (!imageSource || !position || !dimensions) return;
 
-        newImage.push({
-            imageUrl: imageSource,
-            position: position,
-            dimensions: dimensions
+            newImage.push({
+                imageUrl: imageSource,
+                position: position,
+                dimensions: dimensions
+            });
+
+            io.emit('display-image', {
+                imageUrl: imageSource,
+                position: position,
+                dimensions: dimensions
+            });
         });
 
-        io.emit('display-image', {
-            imageUrl: imageSource,
-            position: position,
-            dimensions: dimensions
+        socket.on('update-image', (data) => {
+            const image = newImage.find(img => img.imageUrl === data.imageUrl);
+            if (image) {
+                image.position = data.position;
+                image.dimensions = data.dimensions;
+            }
+            io.emit('sync-image', data);
         });
-    });
 
-    socket.on('update-image', (data) => {
-        const image = newImage.find(img => img.imageUrl === data.imageUrl);
-        if (image) {
-            image.position = data.position;
-            image.dimensions = data.dimensions;
-        }
-        io.emit('sync-image', data);
-    });
-
-    socket.on('remove-image', (imageUrl) => {
-        const index = newImage.findIndex(img => img.imageUrl === imageUrl);
-        if (index !== -1) {
-            newImage.splice(index, 1);
-        }
-        io.emit('update-images', newImage);
+        socket.on('remove-image', (imageUrl) => {
+            const index = newImage.findIndex(img => img.imageUrl === imageUrl);
+            if (index !== -1) {
+                newImage.splice(index, 1);
+            }
+            io.emit('update-images', newImage);
+        });
     });
 }
 
