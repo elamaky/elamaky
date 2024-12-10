1b0642892949370ffdff6dde2c42f7b2fb4587fd
strsljennik <strsljennik@gmail.com>
2024-12-10 20:28:55 +0100

    Update poruke.js

--- poruke.js
+++ poruke.js
@@ -1,49 +1,10 @@
-let io;
-let socket;
-let newImage = [];  
+let io; // Inicijalizujemo io
+let socket; // Inicijalizujemo socket
 
 // Funkcija za setovanje socket-a i io objekta
 function setSocket(serverSocket, serverIo) {
     socket = serverSocket;
     io = serverIo;
-
-    io.on('connection', (socket) => {
-        socket.emit('initial-images', newImage);
-    });
-
-    socket.on('add-image', (imageSource, position, dimensions) => {
-        if (!imageSource || !position || !dimensions) return;
-
-        newImage.push({
-            imageUrl: imageSource,
-            position: position,
-            dimensions: dimensions
-        });
-
-        io.emit('display-image', {
-            imageUrl: imageSource,
-            position: position,
-            dimensions: dimensions
-        });
-    });
-
-    socket.on('update-image', (data) => {
-        const image = newImage.find(img => img.imageUrl === data.imageUrl);
-        if (image) {
-            image.position = data.position;
-            image.dimensions = data.dimensions;
-        }
-        io.emit('sync-image', data);
-    });
-
-
-    socket.on('remove-image', (imageUrl) => {
-        const index = newImage.findIndex(img => img.imageUrl === imageUrl);
-        if (index !== -1) {
-            newImage.splice(index, 1);
-        }
-        io.emit('update-images', newImage);
-    });
 }
 
 // Funkcija za obradu slanja poruka u četu
@@ -65,8 +26,10 @@ function chatMessage(guests) {
 // Funkcija za brisanje chata
 function clearChat() {
     socket.on('clear-chat', () => {
-        io.emit('chat-cleared');
+        console.log(`Zahtev za brisanje chata primljen od ${socket.id}`);
+        io.emit('chat-cleared'); // Emituj svim korisnicima da je chat obrisan
     });
 }
 
+// Eksportovanje funkcija
 module.exports = { setSocket, chatMessage, clearChat };
