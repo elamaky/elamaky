c2eba22419315b2eb75ea3bb2b3b6f195b41b96d
strsljennik <strsljennik@gmail.com>
2024-12-10 13:07:45 +0100

    Update and rename poruke.js to slike.js

---
+++ slike.js
@@ -0,0 +1,51 @@
+let io;
+let socket;
+let newImage = [];  
+
+// Funkcija za setovanje socket-a i io objekta
+function setSocket(serverSocket, serverIo) {
+    socket = serverSocket;
+    io = serverIo;
+
+    io.on('connection', (clientSocket) => {
+        clientSocket.emit('initial-images', newImage);
+    });
+
+    socket.on('add-image', (imageSource, position, dimensions) => {
+        if (!imageSource || !position || !dimensions) return;
+
+        newImage.push({
+            imageUrl: imageSource,
+            position: position,
+            dimensions: dimensions
+        });
+
+        io.emit('display-image', {
+            imageUrl: imageSource,
+            position: position,
+            dimensions: dimensions
+        });
+    });
+
+    socket.on('update-image', (data) => {
+        const image = newImage.find(img => img.imageUrl === data.imageUrl);
+        if (image) {
+            image.position = data.position;
+            image.dimensions = data.dimensions;
+        }
+        io.emit('sync-image', data);
+    });
+
+    socket.on('remove-image', (imageUrl) => {
+        const index = newImage.findIndex(img => img.imageUrl === imageUrl);
+        if (index !== -1) {
+            newImage.splice(index, 1);
+        }
+        io.emit('update-images', newImage);
+    });
+}
+
+// Izvoz modula
+module.exports = {
+    setSocket
+};
