2e34e7b4580af3c6a75695ef7db71ba3fd2c743d
strsljennik <strsljennik@gmail.com>
2024-12-11 01:09:36 +0100

    Update slikemodul.js

--- slikemodul.js
+++ slikemodul.js
@@ -1,8 +1,9 @@
+
 let io;
 let newImage = [];  
 
 // Funkcija za setovanje io objekta
-function setSocket(serverIo, guests) {
+function setSocket(serverIo) {
     io = serverIo;
 
     io.on('connection', (socket) => {
@@ -40,36 +41,6 @@ function setSocket(serverIo, guests) {
             }
             io.emit('update-images', newImage);
         });
-
-        // Obrada slanja poruka u četu
-        socket.on('chatMessage', (msgData) => {
-            const time = new Date().toLocaleTimeString();
-            const messageToSend = {
-                text: msgData.text,
-                bold: msgData.bold,
-                italic: msgData.italic,
-                color: msgData.color,
-                nickname: guests[socket.id], // Korišćenje nadimka za slanje poruke
-                time: time,
-            };
-
-            // Spremi IP, poruku i nickname u fajl
-            saveIpData(socket.handshake.address, msgData.text, guests[socket.id]);
-
-            // Emituj poruku svim korisnicima
-            io.emit('chatMessage', messageToSend);
-        });
-
-        // Funkcija za brisanje chata
-        socket.on('clear-chat', () => {
-            io.emit('chat-cleared');
-        });
-        
-        // Funkcija za čuvanje podataka o IP-u i poruci
-        function saveIpData(ip, message, nickname) {
-            // Ovaj deo koda treba biti implementiran prema vašim potrebama
-            console.log(`IP: ${ip}, Poruka: ${message}, Nickname: ${nickname}`);
-        }
     });
 }
 
