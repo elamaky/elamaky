eec8cba67f2565b85ba5fc77228d7fc5519cfaaa
strsljennik <strsljennik@gmail.com>
2024-12-14 18:04:34 +0100

    Update konobarica.js

--- public/konobarica.js
+++ public/konobarica.js
@@ -29,31 +29,3 @@ const konobaricaItem = document.createElement('div');
 konobaricaItem.classList.add('guest-konobarica');
 konobaricaItem.innerHTML = 'Konobarica'; // Dodajemo samo tekst bez tagova
 guestList.appendChild(konobaricaItem);
-
-
-document.getElementById('mixer').addEventListener('click', () => {
-    // Proveri da li je mixer već otvoren
-    if (!document.getElementById('mixer-container')) {
-        // Dodaj mixer u body
-        const mixerHTML = `
-            <div id="mixer-container">
-                <div id="mixer">
-                    <audio id="audioPlayer" controls style="position: absolute; top: 0px; left: 0;"></audio>
-                    <h2 class="title" style="position: absolute; top: 60px; left: 0;">By *__X__*</h2>
-                    <input type="file" id="fileInput" multiple style="position: absolute; top: 90px; left: 0; right: 0; margin-bottom: 10px;">
-                    <ul id="songList"></ul>
-                    <div id="mixer-buttons">
-                        <button id="playSelected">Play</button>
-                        <button id="deleteSelected">Obriši</button>
-                        <button id="hideButton">Sakrij</button>
-                        <button id="closeButton">Gasi</button>
-                    </div>
-                </div>
-            </div>
-        `;
-        
-        // Dodaj mixer u body
-        document.body.insertAdjacentHTML('beforeend', mixerHTML);
-    }
-});
-
