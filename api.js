e03f8ec3a4faf7b84970e9c14c10c8d01c729c59
strsljennik <strsljennik@gmail.com>
2024-12-15 20:37:38 +0100

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();  // Učitaj sve promenljive iz .env fajla
-
-const { OpenAI } = require('openai');
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,  // Učitaj ključ iz .env fajla
-});
