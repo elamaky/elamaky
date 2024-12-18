3f232af1e20f25d97ab0ca0de21501eb4120f6c3
elamaky <nemirnaela@gmail.com>
2024-12-18 17:58:12 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -3,7 +3,6 @@ let isItalic = false;
 let currentColor = '#FFFFFF';
 let isUnderline = false;  // Dodano za underline
 let isOverline = false;   // Dodano za overline
-let nickname = false;
 
 // Objekat za čuvanje podataka o gostima
 const guestsData = {};
