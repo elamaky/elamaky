ab58e571a33c69557e3f741339ca69f7614ed50f
elamaky <nemirnaela@gmail.com>
2024-12-23 05:21:16 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -33,12 +33,13 @@ document.getElementById('colorPicker').addEventListener('input', function() {
     updateInputStyle();
 });
 
- socket.emit('changeColor', currentColor);
+socket.emit('changeColor', currentColor);
+
 let myGuestElement = document.getElementById(`guest-${nickname}`); // Koristi tvoj trenutni nickname
-    if (myGuestElement) {
-        myGuestElement.style.color = currentColor;
-    
-});
+if (myGuestElement) {
+    myGuestElement.style.color = currentColor;
+}
+
 
 socket.on('colorUpdated', function(guestId, newColor) {
     // Na osnovu guestId, nađeš odgovarajući div u guest listi i ažuriraš boju
