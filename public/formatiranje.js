f651ef93166ffdc1f92bebb77bf9115595a8cbb1
elamaky <nemirnaela@gmail.com>
2024-12-24 01:13:14 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -1,59 +1,3 @@
-let isBold = false;
-let isItalic = false;
-let currentColor = '#FFFFFF';
-let isUnderline = false;  // Dodano za underline
-let isOverline = false;   // Dodano za overline
-
-// Objekat za čuvanje podataka o gostima
-const guestsData = {};
-const colorPrefs = {};
-
-
-// Funkcija za BOLD formatiranje
-document.getElementById('boldBtn').addEventListener('click', function() {
-    isBold = !isBold;
-    updateInputStyle();
-});
-
-// Funkcija za ITALIC formatiranje
-document.getElementById('italicBtn').addEventListener('click', function() {
-    isItalic = !isItalic;
-    updateInputStyle();
-});
-
-// Funkcija za biranje boje
-document.getElementById('colorBtn').addEventListener('click', function() {
-    document.getElementById('colorPicker').click();
-});
-
-// Kada korisnik izabere boju iz palete
-document.getElementById('colorPicker').addEventListener('input', function() {
-    currentColor = this.value;
-    updateInputStyle();
-});
-
-// Funkcija za UNDERLINE formatiranje
-document.getElementById('linijadoleBtn').addEventListener('click', function() {
-    isUnderline = !isUnderline;
-    updateInputStyle();
-});
-
-// Funkcija za OVERLINE formatiranje
-document.getElementById('linijagoreBtn').addEventListener('click', function() {
-    isOverline = !isOverline;
-    updateInputStyle();
-});
-
-
-// Primena stilova na polju za unos
-function updateInputStyle() {
-    let inputField = document.getElementById('chatInput');
-    inputField.style.fontWeight = isBold ? 'bold' : 'normal';
-    inputField.style.fontStyle = isItalic ? 'italic' : 'normal';
-    inputField.style.color = currentColor;
-    inputField.style.textDecoration = (isUnderline ? 'underline ' : '') + (isOverline ? 'overline' : '');
-}
-
 // Kada korisnik pritisne Enter
 document.getElementById('chatInput').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
@@ -103,23 +47,6 @@ socket.on('private_message', function(data) {
     messageArea.prepend(newMessage);
     messageArea.scrollTop = 0; // Automatsko skrolovanje
 });
-
-
-
-// Funkcija za dodavanje stilova gostima
-function addGuestStyles(guestElement, guestId) {
-    const colorPickerButton = document.createElement('input');
-    colorPickerButton.type = 'color';
-    colorPickerButton.classList.add('colorPicker');
-    colorPickerButton.value = guestsData[guestId]?.color || '#FFFFFF'; // Podrazumevana boja
-
-    colorPickerButton.addEventListener('input', function() {
-        guestElement.style.color = this.value;
-        guestsData[guestId].color = this.value; // Ažuriraj boju u objektu
-    });
-
-    guestElement.appendChild(colorPickerButton);
-}
 
 // Kada nov gost dođe
 socket.on('newGuest', function(nickname) {
