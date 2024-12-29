6b3a738b73a091e677cb75836f9c8604e34a8df2
elamaky <nemirnaela@gmail.com>
2024-12-29 06:32:34 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -112,6 +112,17 @@ function updateInputStyle() {
 document.getElementById('chatInput').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
+        let message = this.value;
+        socket.emit('chatMessage', {
+            text: message,
+            bold: isBold,
+            italic: isItalic,
+            color: currentColor,
+            nickname: nickname // Pošalji ime gosta
+        });
+        this.value = ''; // Isprazni polje za unos
+    }
+});
        // Kada server pošalje poruku
 socket.on('chatMessage', function(data) {
     let messageArea = document.getElementById('messageArea');
