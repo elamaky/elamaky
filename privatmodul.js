02b2bdb13197fb3bde79af7ca4ec7bb348fb6f81
elamaky <nemirnaela@gmail.com>
2025-01-02 23:07:05 +0100

    Update privatmodul.js

--- privatmodul.js
+++ privatmodul.js
@@ -1,19 +1,22 @@
 module.exports = function (io, guests) {
     // Privatna poruka
-  io.on('connection', (socket) => {
-    // Postojeći handler za privatne poruke
-    socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
-        const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
-        if (recipientSocketId) {
-            io.to(recipientSocketId).emit('private_message', { message, time, bold, italic, color, underline, overline });
-        }
-    });
+    io.on('connection', (socket) => {
+        // Postojeći handler za privatne poruke
+        socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
+            const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
+            if (recipientSocketId) {
+                io.to(recipientSocketId).emit('private_message', { message, time, bold, italic, color, underline, overline });
+            }
+        });
 
-    // Novi handler za promenu statusa privatnog chata
-    socket.on('toggle_private_chat', (status) => {
-        io.emit('update_private_chat_status', status); // Emituje svim klijentima
+        // Novi handler za promenu statusa privatnog chata
+        socket.on('toggle_private_chat', (status) => {
+            // Emituje svim klijentima informaciju o statusu privatnog chata
+            io.emit('update_private_chat_status', status);
+        });
     });
-});
+};
+
 
             if (recipientSocketId) {
                 // Slanje privatne poruke primaocu
