e91794d0cb0e0ae87706f6ad1e539e6aa5816ddb
elamaky <nemirnaela@gmail.com>
2025-01-02 07:26:27 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -16,3 +16,36 @@ module.exports = (io) => {
         });
     });
 };
+
+socket.on('audioData', (chunk) => {
+  console.log('Received audio data chunk:', chunk);
+
+  // Decode audio data
+  audioContext.decodeAudioData(chunk.buffer, (buffer) => {
+    console.log('Audio data decoded successfully:', buffer);
+
+    // Create a new buffer source and connect to gain node
+    const source = audioContext.createBufferSource();
+    console.log('BufferSource created:', source);
+
+    source.buffer = buffer;
+    source.connect(gainNode);
+    console.log('BufferSource connected to GainNode');
+
+    source.start();
+    console.log('Audio playback started');
+  }, (error) => {
+    console.error('Error decoding audio data:', error);
+  });
+});
+
+socket.on('audioEnded', () => {
+  console.log('Audio playback ended');
+});
+
+function playSong(songUrl) {
+  console.log('Requested to play song with URL:', songUrl);
+
+  socket.emit('playSong', songUrl);
+  console.log('PlaySong event emitted with URL:', songUrl);
+}
