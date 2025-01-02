ff9e9bc14b9a3dcd2ecbcb688ad281dca69c508e
elamaky <nemirnaela@gmail.com>
2025-01-02 23:10:10 +0100

    Update privatmodul.js

--- privatmodul.js
+++ privatmodul.js
@@ -1,7 +1,6 @@
-module.exports = function (io, guests) {
-    // Privatna poruka
+module.exports = function(io, guests) {
     io.on('connection', (socket) => {
-        // Postojeći handler za privatne poruke
+        // Obrada privatnih poruka
         socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
             const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
             if (recipientSocketId) {
@@ -9,16 +8,15 @@ module.exports = function (io, guests) {
             }
         });
 
-        // Novi handler za promenu statusa privatnog chata
+        // Dodajemo ovde samo jedan } koji završava ovu funkciju
         socket.on('toggle_private_chat', (status) => {
-            // Emituje svim klijentima informaciju o statusu privatnog chata
-            io.emit('update_private_chat_status', status);
+            io.emit('update_private_chat_status', status); // Emituje svim klijentima
         });
-    });
-};
+    });  // Ova zagrada zatvara io.on('connection')
+};  // Ova zagrada zatvara modul
 
 
-            if (recipientSocketId) {
+ if (recipientSocketId) {
                 // Slanje privatne poruke primaocu
                 io.to(recipientSocketId).emit('private_message', {
                     from: guests[socket.id],  // Pošiljalac
