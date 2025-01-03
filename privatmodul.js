017bcb0551fb36e5626e17799c2eca21522ff217
elamaky <nemirnaela@gmail.com>
2025-01-03 10:34:54 +0100

    Update privatmodul.js

--- privatmodul.js
+++ privatmodul.js
@@ -1,53 +1,54 @@
-module.exports = function (io, guests, socket) {  // Dodaj "socket" kao parametar
-
-    // Kreiraj varijablu za status privatnog chata
-    let isPrivateChatEnabled = false; // Na početku privatni chat je isključen
-
-    // Kada korisnik uključi ili isključi privatni chat
-    socket.on('toggle_private_chat', (status) => {
-        isPrivateChatEnabled = status; // Ažuriraj status privatnog chata
-        console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-        
-        // Emituj svim povezanim korisnicima
-        io.emit('private_chat_status', isPrivateChatEnabled);
-        console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-    });
-
+module.exports = function (io, guests) {
     // Privatna poruka
-    socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
-        // Proveri da li je privatni chat uključen pre slanja poruke
-        if (!isPrivateChatEnabled) {
-            return console.log('Privatni chat nije uključen');
-        }
+    io.on('connection', (socket) => {
+        // Kreiraj varijablu za status privatnog chata
+        let isPrivateChatEnabled = false; // Na početku privatni chat je isključen
 
-        // Pronalazi socket.id primaoca na osnovu imena
-        const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
+        // Kada korisnik uključi ili isključi privatni chat
+        socket.on('toggle_private_chat', (status) => {
+            isPrivateChatEnabled = status; // Ažuriraj status privatnog chata
+            console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
+            
+            // Emituj svim povezanim korisnicima
+            io.emit('private_chat_status', isPrivateChatEnabled);
+            console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
+        });
 
-        if (recipientSocketId) {
-            // Slanje privatne poruke primaocu
-            io.to(recipientSocketId).emit('private_message', {
-                from: guests[socket.id],  // Pošiljalac
-                message,
-                time,
-                bold,
-                italic,
-                color,
-                underline,
-                overline
-            });
+        // Privatna poruka
+        socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
+            // Proveri da li je privatni chat uključen pre slanja poruke
+            if (!isPrivateChatEnabled) {
+                return console.log('Privatni chat nije uključen');
+            }
 
-            // Slanje privatne poruke pošiljaocu (opciono)
-            socket.emit('private_message', {
-                from: guests[socket.id],  // Pošiljalac (u odgovoru)
-                message,
-                time,
-                bold,
-                italic,
-                color,
-                underline,
-                overline
-            });
-        }
+            // Pronalazi socket.id primaoca na osnovu imena
+            const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
+
+            if (recipientSocketId) {
+                // Slanje privatne poruke primaocu
+                io.to(recipientSocketId).emit('private_message', {
+                    from: guests[socket.id],  // Pošiljalac
+                    message,
+                    time,
+                    bold,
+                    italic,
+                    color,
+                    underline,
+                    overline
+                });
+
+                // Slanje privatne poruke pošiljaocu (opciono)
+                socket.emit('private_message', {
+                    from: guests[socket.id],  // Pošiljalac (u odgovoru)
+                    message,
+                    time,
+                    bold,
+                    italic,
+                    color,
+                    underline,
+                    overline
+                });
+            }
+        });
     });
 };
-
