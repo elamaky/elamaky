a04d58fa26e06e2a7997c443120a125df94d99a7
elamaky <nemirnaela@gmail.com>
2025-01-03 17:04:58 +0100

    Update privatmodul.js

--- privatmodul.js
+++ privatmodul.js
@@ -1,34 +1,22 @@
 module.exports = function (io, guests) {
-    // Privatna poruka
-let isPrivateChatEnabled = false; // Status privatnog chata
+    let isPrivateChatEnabled = false; // Status privatnog chata
 
-io.on('connection', (socket) => {
-    // Pošaljite trenutni status privatnog chata
-    socket.emit('private_chat_status', isPrivateChatEnabled);
+    io.on('connection', (socket) => {
+        // Pošaljite trenutni status privatnog chata
+        socket.emit('private_chat_status', isPrivateChatEnabled);
 
-    // Ako je privatni chat uključen, svi korisnici mogu slati privatne poruke
-    socket.on('private_message', (data) => {
-        if (isPrivateChatEnabled) {
-            // Ovaj deo šalje privatnu poruku određenom korisniku
-            io.to(data.to).emit('private_message', data); // Pod pretpostavkom da koristite socket id kao "to"
-        } else {
-            console.log("Privatni chat nije uključen.");
-        }
-    });
-     
-       // Kada korisnik uključi ili isključi privatni chat
+        // Kada korisnik uključi ili isključi privatni chat
         socket.on('toggle_private_chat', (status) => {
             isPrivateChatEnabled = status; // Ažuriraj status privatnog chata
             console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-            
+
             // Emituj svim povezanim korisnicima
             io.emit('private_chat_status', isPrivateChatEnabled);
             console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
         });
 
-     // Privatna poruka
+        // Osluškuje privatne poruke
         socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
-            // Proveri da li je privatni chat uključen pre slanja poruke
             if (!isPrivateChatEnabled) {
                 return console.log('Privatni chat nije uključen');
             }
@@ -39,7 +27,7 @@ io.on('connection', (socket) => {
             if (recipientSocketId) {
                 // Slanje privatne poruke primaocu
                 io.to(recipientSocketId).emit('private_message', {
-                    from: guests[socket.id],  // Pošiljalac
+                    from: guests[socket.id], // Pošiljalac
                     message,
                     time,
                     bold,
@@ -51,7 +39,7 @@ io.on('connection', (socket) => {
 
                 // Slanje privatne poruke pošiljaocu (opciono)
                 socket.emit('private_message', {
-                    from: guests[socket.id],  // Pošiljalac (u odgovoru)
+                    from: guests[socket.id], // Pošiljalac (u odgovoru)
                     message,
                     time,
                     bold,
@@ -61,6 +49,18 @@ io.on('connection', (socket) => {
                     overline
                 });
             }
+        });
+
+        // Osluškuje ažuriranje selekcije gosta
+        socket.on('update_guest_selection', (data) => {
+            console.log('Ažuriranje selekcije gosta:', data);
+            socket.broadcast.emit('sync_guest_selection', data);
+        });
+
+        // Osluškuje ažuriranje chat inputa
+        socket.on('update_chat_input', (data) => {
+            console.log('Ažuriranje unosa u chat:', data);
+            socket.broadcast.emit('sync_chat_input', data);
         });
     });
 };
