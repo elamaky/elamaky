8c0e2acd80a39192e4908deaadf171bb61d8be47
elamaky <nemirnaela@gmail.com>
2025-01-03 09:03:01 +0100

    Update privatmodul.js

--- privatmodul.js
+++ privatmodul.js
@@ -1,21 +1,22 @@
 module.exports = function (io, guests) {
     // Privatna poruka
-    io.on('connection', (socket) => {
-        socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
-            // Pronalazi socket.id primaoca na osnovu imena
-            const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
+    socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
+        // Pronalazi socket.id primaoca na osnovu imena
+        const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
 
-           // Kada korisnik uključi ili isključi privatni chat
-socket.on('toggle_private_chat', (isPrivateChatEnabled) => {
-    console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-    
-    // Emituj svim povezanim korisnicima, uključujući i trenutnog korisnika
-    io.emit('private_chat_status', isPrivateChatEnabled);
-    console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-});
+        // Kada korisnik uključi ili isključi privatni chat
+        socket.on('toggle_private_chat', (isPrivateChatEnabled) => {
+            console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
+            
+            // Emituj svim povezanim korisnicima, uključujući i trenutnog korisnika
+            io.emit('private_chat_status', isPrivateChatEnabled);
+            console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
+        });
+    });
+};
 
 
-            if (recipientSocketId) {
+                if (recipientSocketId) {
                 // Slanje privatne poruke primaocu
                 io.to(recipientSocketId).emit('private_message', {
                     from: guests[socket.id],  // Pošiljalac
