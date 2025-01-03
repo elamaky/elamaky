d7a0488c73835fdfd42dfddc43716efdc3a857ab
elamaky <nemirnaela@gmail.com>
2025-01-03 23:29:18 +0100

    Update konobaricamodul.js

--- konobaricamodul.js
+++ konobaricamodul.js
@@ -1,11 +1,11 @@
+// konobaricamodul.js
+
 module.exports = (io) => {
     io.on('connection', (socket) => {
-        console.log('A new guest has connected: ', socket.id);
-
         // Kada novi gost uđe, pošaljemo pozdravnu poruku od Konobarice
         socket.on('new_guest', () => {
             const greetingMessage = `Dobro nam došli, osećajte se kao kod kuće, i budite nam raspoloženi! Sada će vam vaša Konobarica posluziti kaficu ☕, 
-                                    a naši DJ-evi će se pobrinuti da vam ispune muzičke želje.`;
+                                                        a naši DJ-evi će se pobrinuti da vam ispune muzičke želje.`;
 
             // Emituj poruku sa klasom 'konobarica' kako bi imala stil
             io.emit('message', { 
@@ -14,3 +14,5 @@ module.exports = (io) => {
                 isSystemMessage: true // označavamo kao sistemsku poruku
             });
         });
+    });
+};
