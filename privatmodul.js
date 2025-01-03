cb8be069570d9aa0d5b7bd19760619eab20779d9
elamaky <nemirnaela@gmail.com>
2025-01-03 09:09:55 +0100

    Update privatmodul.js

--- privatmodul.js
+++ privatmodul.js
@@ -3,17 +3,19 @@ module.exports = function (io, guests) {
     socket.on('private_message', ({ to, message, time, bold, italic, color, underline, overline }) => {
         // Pronalazi socket.id primaoca na osnovu imena
         const recipientSocketId = Object.keys(guests).find(id => guests[id] === to);
+        // Dalja logika za slanje privatne poruke...
+    });
 
-        // Kada korisnik uključi ili isključi privatni chat
-        socket.on('toggle_private_chat', (isPrivateChatEnabled) => {
-            console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-            
-            // Emituj svim povezanim korisnicima, uključujući i trenutnog korisnika
-            io.emit('private_chat_status', isPrivateChatEnabled);
-            console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
-        });
+    // Kada korisnik uključi ili isključi privatni chat
+    socket.on('toggle_private_chat', (isPrivateChatEnabled) => {
+        console.log('Privatni chat:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
+        
+        // Emituj svim povezanim korisnicima
+        io.emit('private_chat_status', isPrivateChatEnabled);
+        console.log('Emitovanje statusa privatnog chata svim korisnicima:', isPrivateChatEnabled ? 'Uključen' : 'Isključen');
     });
 };
+
 
 
                 if (recipientSocketId) {
