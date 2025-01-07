d035525cafdbb1f535a5119338227eaeb4210921
elamaky <nemirnaela@gmail.com>
2025-01-07 07:51:33 +0100

    Update poruke.js

--- public/poruke.js
+++ public/poruke.js
@@ -39,3 +39,18 @@ socket.on('chat-cleared', function() {
     const chatWindow = document.getElementById('messageArea');
     chatWindow.innerHTML = ""; // Briše sve unutar chata
 });
+//ICECAST SERVER
+function playStream() {
+            var audioPlayer = document.getElementById('audioPlayer');
+            var playButton = document.getElementById('playButton');
+            
+            if (audioPlayer.paused) {
+                audioPlayer.style.display = 'block';
+                audioPlayer.play();
+                playButton.textContent = 'Stop Stream';
+            } else {
+                audioPlayer.pause();
+                audioPlayer.style.display = 'none';
+                playButton.textContent = 'Start Stream';
+            }
+        }
