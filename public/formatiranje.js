4a39fd3891732176ffbd49159e6d0091882d73a2
elamaky <nemirnaela@gmail.com>
2025-01-09 09:47:49 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -55,7 +55,7 @@ document.getElementById('chatInput').addEventListener('keydown', function(event)
             color: currentColor,
             underline: isUnderline,
             overline: isOverline,
-            nickname: guests[socket.id],
+            nickname: [socket.id],
       });
         this.value = '';
     }
