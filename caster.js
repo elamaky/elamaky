9049eb1dafb4161039d9018adfeeef1d3049cd8d
elamaky <nemirnaela@gmail.com>
2025-01-09 19:03:20 +0100

    Update caster.js

--- caster.js
+++ caster.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 
-const publicToken = process.env.PUBLIC_TOKEN; 
-const streamUrl = process.env.STREAM_URL;
+const publicToken = '52fd78a9-8b7e-4b0c-aaf1-ab6502ccd171'; // Tvoj public token
+const streamUrl = 'https://sapircast.caster.fm'; // Server hostname za strimovanje
 
+// Funkcija za slanje strimovanih podataka na Caster.fm
 async function streamToCaster() {
   try {
     const response = await axios.post('https://api.caster.fm/stream', JSON.stringify({
@@ -10,14 +11,15 @@ async function streamToCaster() {
       url: streamUrl,
     }), {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json' // Dodajemo header za JSON
       }
     });
 
     console.log('Stream successfully sent to Caster.fm', response.data);
   } catch (error) {
-    console.error('Error streaming to Caster.fm', error);
+    console.error('Error streaming to Caster.fm', error.response ? error.response.data : error.message);
   }
 }
 
+// Izvoz funkcije
 module.exports = streamToCaster;
