aac0f8ac2acc345aa3903effc4009f62705a1d1d
elamaky <nemirnaela@gmail.com>
2025-01-09 07:33:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const bannedUsers = new Set();
 
 // Skladištenje informacija o gostima
 const guests = {};
-const guestsData = guests;
+const guestsData = {};
 const assignedNumbers = new Set(); // Set za generisane brojeve
 
 // Dodavanje socket događaja iz banmodula
