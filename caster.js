8a7f392157b243b993582f5a90227220af4ff54c
elamaky <nemirnaela@gmail.com>
2025-01-09 18:03:21 +0100

    Create caster.js

---
+++ caster.js
@@ -0,0 +1,22 @@
+// caster.js
+const axios = require('axios');
+
+const publicToken = '52fd78a9-8b7e-4b0c-aaf1-ab6502ccd171'; // Tvoj public token
+const streamUrl = 'http://your-stream-url.com'; // URL tvoje strimovane muzike ili zvuka
+
+// Funkcija za slanje strimovanih podataka na Caster.fm
+async function streamToCaster() {
+  try {
+    const response = await axios.post('https://api.caster.fm/stream', {
+      token: publicToken,
+      url: streamUrl,
+    });
+
+    console.log('Stream successfully sent to Caster.fm', response.data);
+  } catch (error) {
+    console.error('Error streaming to Caster.fm', error);
+  }
+}
+
+// Izvoz funkcije
+module.exports = streamToCaster;
