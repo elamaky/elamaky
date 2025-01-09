34a0d44b0deac48d2d4dd0774a8839c0c8533d1f
elamaky <nemirnaela@gmail.com>
2025-01-09 04:24:38 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -158,7 +158,7 @@ if (Array.isArray(users)) {
         }
     }
 });
-    });
+    }
 function setGuestColor(guestId, color) {
     const guestElement = document.getElementById(guestId);
     if (guestElement) {
